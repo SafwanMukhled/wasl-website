f63e305a89944bd8dc2fcedaa66d7e8aeda1f632
SafwanMukhled <161850627+SafwanMukhled@users.noreply.github.com>
2025-10-17 10:22:46 +0300

    include.js

--- include.js
+++ include.js
@@ -1,16 +1,17 @@
 // تحميل الهيدر والفوتر تلقائياً
 document.addEventListener("DOMContentLoaded", function() {
     // تحميل الهيدر
-    fetch("../1-HTML/Parts/header.html")
+    fetch("header.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("header").innerHTML = data;
         });
 
     // تحميل الفوتر
-    fetch("../1-HTML/Parts/footer.html")
+    fetch("footer.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("footer").innerHTML = data;
         });
 });
+
